1fe916806f26e4f18e099d9be17af8d5c1e1ded3
Cody Keener <codykeener@Codys-MBP.localdomain>
2025-01-06 16:20:07 -0600

    Added some cool backgrounds that change depending on the weather

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@
 
 // any component that uses useAuth needs this because if a component directly imports useAuth, it needs to be a client component since useAuth uses React hooks.
 
-// import { Button } from 'react-bootstrap';
 // import { signOut } from '@/utils/auth'; // anything in the src dir, you can use the @ instead of relative paths
 import { useAuth } from '@/utils/context/authContext';
 import WeatherForm from '../components/forms/WeatherForm';
@@ -11,22 +10,22 @@ function Home() {
   const { user } = useAuth();
 
   return (
-    <div
-      // className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        // height: '90vh',
-        // padding: '30px',
-        maxWidth: '400px',
-        margin: '20px auto',
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: 'column',
-      }}
-    >
-      <h2>Hello, {user.displayName}! </h2>
-      <h5>Welcome to Fetch Weather</h5>
-      <div style={{ marginTop: '20px' }}>
-        <WeatherForm />
+    <div id="main-app" style={{ width: '100%', minHeight: '860px' }}>
+      <div
+        style={{
+          maxWidth: '400px',
+          margin: '0px auto',
+          paddingTop: '20px',
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <h2>Hello, {user.displayName}! </h2>
+        <h5>Welcome to Fetch Weather</h5>
+        <div style={{ marginTop: '20px' }}>
+          <WeatherForm />
+        </div>
       </div>
     </div>
   );
